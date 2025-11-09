1bb00cafcf6022776faf9a4f84bc86013eea3399
PeerBasha <smdpeeru@gmail.com>
2025-11-09 23:40:22 +0530

    terraform

---
+++ data-sources/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "terraform" {
+    ami = data.aws_ami.joindevops.id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+    tags = {
+      Name = "Terraform"
+      Terraform = "True"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+  name   = "allow-all"
+  
+  egress {
+    from_port        = 0 # 0 from port 0 to 0 port means all ports
+    to_port          = 0
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] #internet
+}
+
+  ingress {
+    from_port        = 0 # 0 from port 0 to 0 port means all ports
+    to_port          = 0
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] #internet
+}
+
+tags = {
+  Name = "allow=all"
+}
+
+}
